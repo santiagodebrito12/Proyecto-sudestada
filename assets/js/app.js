efdbfd04804f5bd38150c814c339d19704c68ed3
santiagodebrito12 <santiago.debrito@live.com>
2021-06-07 10:47:16 -0300

    agregue version mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,31 @@ btnDerecha.addEventListener('click', () =>{
 btnIzquierda.addEventListener('click', () =>{
     contenedorCarrousel.scrollLeft -= contenedorCarrousel.offsetWidth;
 })
+
+
+
+//funcionalidad del nav Bar
+const contenedorNavBar = document.querySelector('.contenedor-burguer-menu');
+const btnNavBar = document.querySelector('.contenedor-navbar');
+const navMenu   = document.querySelector('#nav-menu');
+
+
+//Evento CLICK
+
+btnNavBar.addEventListener('click', () =>{
+    
+    if(navMenu.classList.contains('close')){
+        
+        navMenu.classList.remove('close');
+        contenedorNavBar.classList.add('open');
+        navMenu.classList.add('open','animate__animated' ,'animate__slideInDown');
+     
+    }else{
+        navMenu.classList.add('close','animate__animated','slideInUp');
+       contenedorNavBar.classList.remove('open','animate__animated' ,'animate__slideInDown');
+       navMenu.classList.remove('open');
+    }
+
+})
+
+//animacion de imagenes
